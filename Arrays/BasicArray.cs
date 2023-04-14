fe0b86b80cc09e6b978d34583f4d6529a54bb061
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-14 23:33:56 +0530

    [Sachin] Adding Programs

---
+++ Arrays/BasicArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class BasicArray
+    {
+        public void Element()
+        {
+            string[] person = new string[5];
+            person[0] = "Rahul";
+            person[1] = "Ajay";
+            person[2] = "Pramod";
+            person[3] = "Shantanu";
+            person[4] = "Anish";
+
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.WriteLine(person[i]);
+            }
+        }
+    }
+}
+
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -5,9 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Array Program");
+            Console.WriteLine("Please select any one program");
+            Console.WriteLine("1:Addelement\n.2BasicArray");
+            int option = Convert.ToInt32(Console.ReadLine());
 
-            AddingElement myObj = new AddingElement();
-            myObj.AddElementFunction();
+            switch (option)
+            {
+                case 1:
+                    AddingElement myObj = new AddingElement();
+                    myObj.AddElementFunction();
+                    break;
+                case 2:
+                    BasicArray myObj1 = new BasicArray();
+                    myObj1.Element();
+                    break;
+            }
+
+           
         }
     }
 }
